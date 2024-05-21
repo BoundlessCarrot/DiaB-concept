fb0133dcaa3b5fa28f40488bfc47728d4d632aa0
Jordan Streete <jordan.streete@marleyspoon.com>
2024-05-21 18:07:43 +0200

    Working enemy spawning!

--- src/main.zig
+++ src/main.zig
@@ -46,31 +46,33 @@ fn calculatePathEnemyToPlayer(player: raylib.Vector2, enemy: raylib.Vector2) ray
     return raylib.Vector2.init(x, y);
 }
 
-fn spawnEnemy(missedShotCoords: raylib.Vector2, enemyList: std.ArrayList(raylib.Rectangle)) void {
+fn spawnEnemy(missedShotCoords: raylib.Vector2, enemyList: *std.ArrayList(raylib.Rectangle)) !void {
     const rec = raylib.Rectangle.init(missedShotCoords.x, missedShotCoords.y, 30, 30);
-    enemyList.append(rec);
+    try enemyList.append(rec);
     // raylib.drawRectangleRec(rec, raylib.Color.red);
 }
 
-fn drawEnemies(enemyList: std.ArrayList(raylib.Rectangle)) void {
-    for (enemyList.toOwnedSlice()) |enemy| {
+fn drawEnemies(enemyList: *std.ArrayList(raylib.Rectangle)) void {
+    for (enemyList.items) |enemy| {
         raylib.drawRectangleRec(enemy, raylib.Color.red);
     }
 }
 
-fn updateEnemyPos(player: raylib.Vector2, enemyList: std.ArrayList(raylib.Rectangle)) void {
-    for (enemyList.toOwnedSlice()) |rec| {
-        const slope = ((player.y - rec.y) / (player.x - rec.x)) / 5;
-        rec.x += slope;
-        rec.y += slope;
+// TODO: updating isn't working completely correctly
+//  direction and speed are wrong
+//  coords need to be clamped to stay on the screen
+fn updateEnemyPos(player: raylib.Vector2, enemyList: *std.ArrayList(raylib.Rectangle)) void {
+    for (0..enemyList.items.len) |i| {
+        const rec = enemyList.items[i];
+        const slope = ((player.y - rec.y) / (player.x - rec.x));
+        // _ = @constCast(&enemyList.items[i]);
+        enemyList.items[i] = raylib.Rectangle.init(rec.x + slope, rec.y + slope, rec.width, rec.height);
     }
 }
 
-// TODO: this is a bad way to do this, but it works for now
-//  return type for anonymous struct?
-fn checkCollisionLineRec(enemyList: std.ArrayList(raylib.Rectangle), line_start: raylib.Vector2, line_end: raylib.Vector2) CollisionTup {
+fn checkCollisionLineRec(enemyList: *std.ArrayList(raylib.Rectangle), line_start: raylib.Vector2, line_end: raylib.Vector2) CollisionTup {
     var tup = CollisionTup{ .bool = false, .point = undefined, .rec_idx = undefined };
-    for (enemyList.toOwnedSlice(), 0..) |rec, i| {
+    outer: for (enemyList.items, 0..) |rec, i| {
         var x: f32 = rec.x;
         while (x <= rec.x + rec.width) : (x += 1.0) {
             var y: f32 = rec.y;
@@ -78,7 +80,7 @@ fn checkCollisionLineRec(enemyList: std.ArrayList(raylib.Rectangle), line_start:
                 const collision_point = raylib.Vector2.init(x, y);
                 if (raylib.checkCollisionPointLine(collision_point, line_start, line_end, 1)) {
                     tup = CollisionTup{ .bool = true, .point = collision_point, .rec_idx = i };
-                    return tup;
+                    break :outer;
                 }
             }
         }
@@ -88,7 +90,7 @@ fn checkCollisionLineRec(enemyList: std.ArrayList(raylib.Rectangle), line_start:
 }
 
 pub fn main() anyerror!void {
-    raylib.initWindow(screenWidth, screenHeight, "raylib [core] testing - basic window w/ kb input, window limits, mouse line tracking, and bullet collision detection");
+    raylib.initWindow(screenWidth, screenHeight, "Death is a Blessing concept test");
     defer raylib.closeWindow();
 
     var numCollisions: usize = 0;
@@ -107,6 +109,9 @@ pub fn main() anyerror!void {
     defer enemyList.deinit();
 
     raylib.setTargetFPS(60);
+
+    const rec = raylib.Rectangle.init(900, 600, 30, 30);
+    try enemyList.append(rec);
 
     while (!raylib.windowShouldClose()) {
         // Get input and update
@@ -132,7 +137,7 @@ pub fn main() anyerror!void {
 
         // raylib.hideCursor();
 
-        const string = try std.fmt.allocPrintZ(test_alloc, "move da ball, x: {d}, y: {d}, hits: {d}", .{ ballPos.x, ballPos.y, numCollisions });
+        const string = try std.fmt.allocPrintZ(test_alloc, "move da ball, x: {d}, y: {d}, #. enemies: {d}, hits: {d}", .{ ballPos.x, ballPos.y, enemyList.items.len, numCollisions });
         defer test_alloc.free(string);
 
         raylib.clearBackground(raylib.Color.white);
@@ -140,23 +145,21 @@ pub fn main() anyerror!void {
         raylib.drawCircleV(ballPos, radius, raylib.Color.green);
         // raylib.drawLineV(ballPos, mouse_line_aim, raylib.Color.gray);
 
-        const rec = raylib.Rectangle.init(900, 600, 30, 30);
-        try enemyList.append(rec);
-        drawEnemies(enemyList);
-        updateEnemyPos(ballPos, enemyList);
+        drawEnemies(&enemyList);
+        updateEnemyPos(ballPos, &enemyList);
 
         // check for collision between shot path and target
         if (raylib.isMouseButtonReleased(raylib.MouseButton.mouse_button_left)) {
-            const collision = checkCollisionLineRec(enemyList, ballPos, mouse_line_end);
+            const collision = checkCollisionLineRec(&enemyList, ballPos, mouse_line_end);
             if (collision.bool == true) {
                 numCollisions += 1;
                 raylib.drawText("COLLISION!", 900, 10, 20, raylib.Color.green);
                 raylib.drawLineV(ballPos, collision.point, raylib.Color.dark_green);
-                _ = try enemyList.swapRemove(collision.rec_idx);
+                _ = enemyList.orderedRemove(collision.rec_idx);
                 // continue;
             } else {
                 raylib.drawLineV(ballPos, mouse_line_end, raylib.Color.gray);
-                spawnEnemy(mouse_line_end);
+                try spawnEnemy(mouse_line_end, &enemyList);
             }
         }
     }
